94ca759648076b9c43726c9925cb1550f9bb737a
Lukas Nabergall <Lukas Nabergall>
2025-06-12 02:17:05 -0400

    Added a configuration

--- configs.py
+++ configs.py
@@ -217,6 +217,33 @@ CONFIGS = {
         "particle_limit": 120,
         "boundstate_limit": 160,
 
-        "field_preloads": 10,
+        "field_preloads": 100,
+    },
+    "base_1gamma_large": {
+        "name": "base_1gamma_large",
+        "n": 248,    # multiple of 4
+        "k": 2500,
+        "t": 2,
+        "N": jnp.array((2000, 500)),
+        "T_M": jnp.array((1, 2)),
+        "T_Q": jnp.array((-1, 1)),
+
+        "beta": 16.0,
+        "gamma": 1.0,
+
+        "mu": 1.0,
+
+        "alpha": 1.0,
+
+        "epsilon": 1.0,
+        "delta": 2,
+
+        "pad_value": 5000,
+        "emission_streams": 100,
+        "boundstate_streams": 100,
+        "particle_limit": 500,
+        "boundstate_limit": 800,
+
+        "field_preloads": 100,
     },
 }
